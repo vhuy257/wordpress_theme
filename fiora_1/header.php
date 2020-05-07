acbbee01ff1939b642a797d968e49f7da47edd11
huy <huy.nguyen@monimedia.com>
2020-05-07 14:22:14 +0700

    push theme fiora

--- fiora_1/header.php
+++ fiora_1/header.php
@@ -1,1 +1,30 @@
-header
+<?php
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Fiora 01
+ * @since Fiora 1.0
+ */
+
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+	<?php
+	// Display the Custom Logo
+	the_custom_logo();
+	// No Custom Logo, just display the site's name
+	if (!has_custom_logo()) {
+		?>
+		<h1><?php bloginfo('name'); ?></h1>
+		<?php
+	}
+	?>
